1a5414ddac70c6d74e6a2fbbe4bd725762800a1d
DuongPH14 <duong.pham@cerebral.com>
2022-05-30 23:55:04 +0700

    submit project

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=duongpham910/ml-microservice-api:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-microservice-api --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/ml-microservice-api-b5994c9f5-dzpfg --address 0.0.0.0 8000:80
